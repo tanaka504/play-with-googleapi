9b5f90502b516cfc58742527c8a86ad92cca94e3
tanaka504 <tanan2-o-univ.kisasage@outlook.jp>
2018-11-10 01:59:59 +0900

    ocr using gdrive

---
+++ ocr_gdrive.py
@@ -0,0 +1,85 @@
+from __future__ import print_function
+import httplib2
+import os
+import io
+
+from googleapiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
+import time
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/drive-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/drive'
+CLIENT_SECRET_FILE = './data/client_secret.json'
+APPLICATION_NAME = 'Drive API Python Quickstart'
+
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    credential_path = os.path.join("./", 'drive-python-quickstart.json')
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else:  # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+
+def OCR(filename):
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('drive', 'v3', http=http)
+    imgfile = os.path.join('tmp_png', filename)
+    outfile = os.path.join('tmp_png', filename[:-4]+'.txt')  # Text file outputted by OCR
+
+    mime = 'application/vnd.google-apps.document'
+    res = service.files().create(
+        body={
+            'name': imgfile,
+            'mimeType': mime
+        },
+        media_body=MediaFileUpload(imgfile, mimetype=mime, resumable=True)
+    ).execute()
+
+    downloader = MediaIoBaseDownload(
+        io.FileIO(outfile, 'wb'),
+        service.files().export_media(fileId=res['id'], mimeType="text/plain")
+    )
+    done = False
+    while done is False:
+        status, done = downloader.next_chunk()
+
+    service.files().delete(fileId=res['id']).execute()
+    print("Done.")
+
+def main():
+    start_time = time.time()
+    files = [f for f in os.listdir('tmp_png') if os.path.isfile(os.path.join('tmp_png', f))]
+    [OCR(filename) for filename in files]
+
+    exec_time = time.time() - start_time
+    print('Finish |exec time:{0}'.format(exec_time) + '[sec]')
+
+if __name__ == '__main__':
+    main()
